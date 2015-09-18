b066e6fd45143a433c85dda846ad88bccd246b67
tlainevool <tlainevool@gmail.com>
2015-09-17 23:46:23 -0700

    Added data caching.

--- cachematrix.R
+++ cachematrix.R
@@ -9,14 +9,16 @@
 #   A special "matrix" object that can be used bt the "cacheSolve" function
 #       to cache and retrieve the inverse
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
+        i <- NULL
         set <- function(y) {
                 x <<- y
-                m <<- NULL
+                i <<- NULL
         }
         get <- function() x
-
-        list(set = set, get = get)
+        setinverse <- function(inverse) i <<- inverse
+        getinverse <- function() i
+        list(set = set, get = get,
+             getinverse = getinverse, setinverse = setinverse)
 }
 
 
@@ -30,5 +32,13 @@ makeCacheMatrix <- function(x = matrix()) {
 # Returns:
 #   A matrix that is the inverse of 'x'.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getinverse()
+        if( !is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setinverse(i)
+        i
 }
--- tests/testthat/test_caching.R
+++ tests/testthat/test_caching.R
@@ -4,8 +4,15 @@ test_that("The makeCacheMatrix function returns a list", {
 })
 
 test_that("The get and set functions work", {
-        expected <- matrix(1:6, nrow = 3, ncol = 2)
+        expected <- matrix(1:9, nrow = 3, ncol = 3)
         x <- makeCacheMatrix(expected)
 
         expect_equal(x$get(), expected)
+})
+
+test_that("The inverse can be gotten from the 'matrix'", {
+        original <- matrix(1:4, nrow = 2, ncol = 2)
+        x <- makeCacheMatrix(original)
+
+        expect_equal(cacheSolve(x), solve(original))
 })
